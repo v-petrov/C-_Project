83ce74c5bfd8be5a01a22578c7b4bdaabc01c419
v-petrov <vapetrov@tu-sofia.bg>
2023-11-08 10:33:03 +0200

    changes in .h and .cpp in flight

--- Project/flight.cpp
+++ Project/flight.cpp
@@ -57,3 +57,53 @@ string Flight::getTime() {
 int Flight::getPlaneId() const {
     return this->plainId;
 }
+
+nlohmann::json Flight::toJson() const {
+    nlohmann::json jsonFlight;
+    jsonFlight["id"] = id;
+    jsonFlight["status"] = status;
+    jsonFlight["startingDestination"] = startingDestination;
+    jsonFlight["endingDestination"] = endingDestination;
+    jsonFlight["totalDistance"] = totalDistance;
+    jsonFlight["date"] = date;
+    jsonFlight["time"] = time;
+    jsonFlight["plainId"] = plainId;
+    return jsonFlight;
+}
+
+void from_json(const nlohmann::json& json, Flight& flight) {
+    json.at("id").get_to(flight.id);
+    json.at("status").get_to(flight.status);
+    json.at("startingDestination").get_to(flight.startingDestination);
+    json.at("endingDestination").get_to(flight.endingDestination);
+    json.at("totalDistance").get_to(flight.totalDistance);
+    json.at("date").get_to(flight.date);
+    json.at("time").get_to(flight.time);
+    json.at("plainId").get_to(flight.plainId);
+}
+
+ostream& operator<<(ostream& out, Flight flight) {
+    out << "ID: " << flight.getId() << ", ";
+    out << "Status: ";
+    switch (flight.getFlightStatus()) {
+        case 0:
+            out << "finished" << ", ";
+            break;
+        case 1:
+            out << "ongoing" << ", ";
+            break;
+        case 2:
+            out << "incoming" << ", ";
+            break;
+        case 3:
+            break;
+    }
+    out << "Starting Destination: " << flight.getStartingDestination() << ", ";
+    out << "Ending Destination: " << flight.getEndingDestination() << ", ";
+    out << "Total distance(km): " << flight.getTotalDistance() << ", ";
+    out << "Date: " << flight.getDate() << ", ";
+    out << "Time: " << flight.getTime() << ".\n";
+    out << "PLANE INFORMATION: \n";
+    Plane::printPlane("id", to_string(flight.getPlaneId()));
+    return out;
+}
